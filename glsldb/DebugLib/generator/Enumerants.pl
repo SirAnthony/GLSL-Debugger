3f1e320ff794e420546b81ab90a87e243f621c79
Sir Anthony <anthony@adsorbtion.org>
2014-02-16 01:18:56 +0600

    new Enumerants

--- glsldb/DebugLib/generator/Enumerants.pl
+++ glsldb/DebugLib/generator/Enumerants.pl
@@ -1,11 +1,6 @@
-#!/usr/bin/perl
-# This file is refactoring result of old 3 scripts gen*Enumerant.
-# Now it requires -m parameter with mode of source file.
-# Mode can be gl, glx or wgl. gl is default.
-
 ################################################################################
 #
-# Copyright (c) 2013 SirAnthony <anthony at adsorbtion.org>
+# Copyright (c) 2014 SirAnthony <anthony at adsorbtion.org>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -27,36 +22,49 @@
 #
 ################################################################################
 
-
+use strict;
+use warnings;
 use Getopt::Std;
-require genTools;
-our %files;
-our %regexps;
+use genTools;
 our %extnames_defines;
 our @problem_defines;
+our $opt_p;
+getopt('p');
 
-our $opt_m = "gl";
-getopt('m');
+my %problem_defines_map = map {$_ => 1} @problem_defines;
+
+
+sub generate_element
+{
+    my $name = shift;
+    my $out = "\t{$name, \"$name\"},";
+    if ($problem_defines_map{$name}) {
+        $out = "#ifdef $name
+$out
+#endif /* $name */";
+    }
+    return $out;
+}
+
+sub check_define {
+    my ($extname, $elements) = @_;
+    my $out = join("\n", map generate_element($_), @$elements);
+    if ($extname) {
+        $out = "
+#ifdef $extname
+$out
+#endif /* $extname */
+";
+    }
+    return $out;
+}
 
 sub out_struct {
-    my $name = shift;
+    my ($name, $order, $enums) = @_;
     my $elements = "";
-    sub generate_element {
-        my $name = $_;
-        my $out = "";
-        my $need_escape = scalar grep { /^$name$/ } @problem_defines;
-        $out .= "#ifdef $name\n" if $need_escape;
-        $out .= "\t{$name, \"$name\"},";
-        $out .= "\n#endif /* $name */" if $need_escape;
-        return $out;
-    }
-
-    foreach my $subarray (@_){
-        my ($extname, $enums) = (@$subarray);
-        next if not @$enums;
-        $elements .= "\n#ifdef $extname\n" if $extname;
-        $elements .= join("\n", map generate_element, @$enums);
-        $elements .= "\n#endif /* $extname */\n" if $extname;
+    foreach my $extname (@$order){
+        next if not defined $enums->{$extname};
+        $elements .= check_define($extname, $enums->{$extname});
     }
     print "
 static struct {
@@ -67,56 +75,49 @@ $elements
 \t{0, NULL}
 };
 ";
+
 }
 
-sub out {
-    if ($opt_m eq "glx") {
-        out_struct("glxEnumerantsMap", @_);
-    } elsif ($opt_m eq "wgl") {
-        out_struct("wglEnumerantsMap", @_);
-    } else {
-        my @enums;
-        my @bits;
-        foreach my $subarray (@_){
-            my ($extname, $elements) = (@$subarray);
-            my @e = grep(!/GL_FALSE|GL_TRUE|GL_TIMEOUT_IGNORED/, @$elements);
-            my @b = grep(/_BIT$|_BIT_\w+$|_ATTRIB_BITS/, @$elements);
-            push @enums, [$extname, \@e];
-            push @bits, [$extname, \@b];
-        }
-        out_struct("glEnumerantsMap", @enums);
-        # create OpenGL Bitfield map
-        out_struct("glBitfieldMap", @bits);
-    }
-}
-
-my @matches;
-my $last_ext = "";
-sub push_matches
+sub out
 {
-    my ($line, $extname, $match) = (@_);
-    $extname = $extnames_defines{$extname} if defined $extnames_defines{$extname};
-    if ($last_ext ne $extname){
-        push @matches, [$extname, []];
-        $last_ext = $extname;
+    my ($order, $enums) = @_;
+    foreach my $struct_name (keys %$enums) {
+        out_struct($struct_name, $order, $enums->{$struct_name});
     }
-    $arr = ${$matches[-1]}[-1];
-    push @$arr, $match;
 }
 
-my %modes = (
-    "gl" => ["GL_VERSION_1_0", "GL_", {$regexps{"glvar"} => \&push_matches}],
-    "glx" => ["GLX_VERSION_1_0", "GLX_", {$regexps{"glxvar"} => \&push_matches}],
-    "wgl" => ["WGL_VERSION_1_0", "WGL_", {$regexps{"wglvar"} => \&push_matches}],
-);
+sub convert
+{
+    my $input = shift;
+    my $enumerants = {};
+    my $extenumerants = {};
+    my $bits = {};
+    my $keys_order = [];
 
-if (not defined $modes{$opt_m}) {
-    die "Mode must be one of " . join(", ", keys %modes) . "\n";
+    foreach my $define (@$input) {
+        my ($isExtension, $ext, $enum) = @$define;
+        next if $enum =~ /GL_FALSE|GL_TRUE|GL_TIMEOUT_IGNORED/;
+
+        $ext = $extnames_defines{$ext} if defined $extnames_defines{$ext};
+        push @$keys_order, $ext unless grep{$_ eq $ext} @$keys_order;
+
+        # TODO: there is BIT enums in GLX, do we need them as extBitfield?
+        my $array = ($enum =~ m/^(?:GLX|WGL)_/ ) ? $extenumerants :
+                    ($enum =~ /_BIT$|_BIT_\w+$|_ATTRIB_BITS/) ? $bits :
+                        $enumerants;
+        $array->{$ext} = [] unless $array->{$ext};
+        push @{$array->{$ext}}, $enum;
+    }
+
+    return ($keys_order, {
+        "glEnumerantsMap" => $enumerants,
+        "extEnumerantsMap" => $extenumerants,
+        "glBitfieldMap" => $bits
+    });
 }
 
-foreach my $filename (@{$files{$opt_m}}) {
-    parse_output($filename, @{$modes{$opt_m}}, 1);
-}
+require "$opt_p/glheaders.pm";
+our @defines;
 
-header_generated();
-out(@matches);
+header_generated("#");
+out(convert(\@defines));
